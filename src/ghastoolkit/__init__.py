f5b67caff6349f86b5c828b16688164dfbf1f1b2
GeekMasher <geekmasher@gmail.com>
2023-06-06 19:06:20 +0100

    feat: v0.4.0

--- src/ghastoolkit/__init__.py
+++ src/ghastoolkit/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "ghastoolkit"
 __title__ = "GHAS Toolkit"
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 __description__ = "GitHub Advanced Security Python Toolkit"
 __summary__ = """\
