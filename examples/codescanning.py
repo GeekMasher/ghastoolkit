45bc78c2b8b22f6e6fe4defccf1f09668fdb99b2
GeekMasher <geekmasher@gmail.com>
2023-07-13 17:51:03 +0100

    feat: update codescanning example

--- examples/codescanning.py
+++ examples/codescanning.py
@@ -1,17 +1,31 @@
 
+import os
 from ghastoolkit import GitHub, CodeScanning
 
-GitHub.init("GeekMasher/ghastoolkit")
+GitHub.init(
+    os.environ.get("GITHUB_REPOSITORY", "GeekMasher/ghastoolkit"),
+)
+print(f" >> {GitHub.repository}")
 
 cs = CodeScanning()
 
-# Get all alerts
-alerts = cs.getAlerts("open")
-print(f"Alert Count :: {len(alerts)}")
+alerts = cs.getAlerts("open", ref=GitHub.repository.reference)
 
-
+# Get list of the delta alerts in a PR
 if GitHub.repository.isInPullRequest():
-    # Get list of the delta alerts in a PR
-    delta = cs.getAlertsInPR("refs/heads/master")
+    delta = cs.getAlertsInPR("refs/heads/main")
     print(f"Delta :: {len(delta)}")
+
+# Get all alerts
+else:
+    alerts = cs.getAlerts("open")
+    print(f"Alert Count :: {len(alerts)}")
+
+# get latest analyses for each tool 
+analyses = cs.getLatestAnalyses()
+print(f"Analyses :: {len(analyses)}")
+
+# get list of tools
+tools = cs.getTools()
+print(f"Tools :: {tools}")
 
