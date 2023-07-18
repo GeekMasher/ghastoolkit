6d19adcb06b1af947f84cb4f2134803177327edc
GeekMasher <geekmasher@gmail.com>
2023-07-18 14:45:14 +0100

    feat: update code scanning packs support

--- src/ghastoolkit/codeql/packs/__main__.py
+++ src/ghastoolkit/codeql/packs/__main__.py
@@ -3,6 +3,7 @@ import os
 import logging
 from argparse import ArgumentParser
 from ghastoolkit import __banner__
+from ghastoolkit.codeql.packs.pack import CodeQLPack
 from ghastoolkit.codeql.packs.packs import CodeQLPacks
 from ghastoolkit.octokit.codescanning import CodeScanning
 from ghastoolkit.octokit.github import GitHub
@@ -21,6 +22,9 @@ parser.add_argument(
     "-dd", "--display-dependencies", action="store_true", help="Display Dependencies"
 )
 
+parser_bumper = parser.add_argument_group("Bumper")
+parser_bumper.add_argument("-b", "--bump", default="minor", help="Bump version")
+
 arguments = parser.parse_args()
 remote = True if arguments.repository else False
 
@@ -33,24 +37,46 @@ else:
 
 code_scanning = CodeScanning()
 
+if arguments.bump:
+    # version bump mode
+    logging.info(f"Bumping CodeQL Pack Versions")
+    logging.info(f" - Bump: {arguments.bump}")
 
-logging.debug(f"Loading packs from environment...")
+    if os.path.isdir(arguments.packs_path):
+        packs = CodeQLPacks(arguments.packs_path)
+        for pack in packs:
+            old_version = pack.version
+            v = pack.updateVersion(arguments.bump)
+            logging.info(f"Pack :: {pack.name} - {old_version} -> {v}")
+            pack.updatePack()
 
-packs = CodeQLPacks()
-packs.load(arguments.packs_path)
+    elif os.path.isfile(arguments.packs_path):
+        pack = CodeQLPack(arguments.packs_path)
+        old_version = pack.version
+        new_version = pack.updateVersion(arguments.bump)
+        logging.info(f"Pack :: {pack.name} - {old_version} -> {new_version}")
 
-logging.info(f"CodeQL Packs :: {len(packs)}")
-logging.info("")
+        pack.updatePack()
 
-for pack in packs:
-    logging.info(f" - {pack}")
+else:
+    # display-only mode
+    logging.debug(f"Loading packs from environment...")
 
-    if remote:
-        remote_version = pack.remote_version
-        if remote_version:
-            logging.info(f"   |> Remote Version: `{pack.remote_version}`")
+    packs = CodeQLPacks()
+    packs.load(arguments.packs_path)
 
-    if arguments.display_dependencies:
-        logging.info(f"   |> Dependencies")
-        for dep in pack.dependencies:
-            logging.info(f"   |--> {dep}")
+    logging.info(f"CodeQL Packs :: {len(packs)}")
+    logging.info("")
+
+    for pack in packs:
+        logging.info(f" - {pack}")
+
+        if remote:
+            remote_version = pack.remote_version
+            if remote_version:
+                logging.info(f"   |> Remote Version: `{pack.remote_version}`")
+
+        if arguments.display_dependencies:
+            logging.info(f"   |> Dependencies")
+            for dep in pack.dependencies:
+                logging.info(f"   |--> {dep}")
