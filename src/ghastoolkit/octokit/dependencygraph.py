392608713171901bc00d0182b0bc2bebdfcfd205
GeekMasher <geekmasher@gmail.com>
2023-07-31 13:57:30 +0100

    feat: add better logging to failing PR

--- src/ghastoolkit/octokit/dependencygraph.py
+++ src/ghastoolkit/octokit/dependencygraph.py
@@ -124,12 +124,16 @@ class DependencyGraph:
         head = urllib.parse.quote(head, safe="")
         basehead = f"{base}...{head}"
         logger.debug(f"PR basehead :: {basehead}")
+
         results = self.rest.get(
             "/repos/{owner}/{repo}/dependency-graph/compare/{basehead}",
             {"basehead": basehead},
             expected=200,
         )
+
         if not results:
+            logger.warning("Failed to get dependencies from Pull Request")
+            logger.warning("Make sure Advanced Security is enabled on the repository")
             return dependencies
 
         for depdata in results:
