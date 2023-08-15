4653697ca5f32b9651a50996a733b702ef44ec10
GeekMasher <geekmasher@gmail.com>
2023-08-15 10:10:13 +0100

    feat: add CodeQL pack publish command

--- src/ghastoolkit/codeql/__main__.py
+++ src/ghastoolkit/codeql/__main__.py
@@ -2,7 +2,26 @@
 import logging
 from argparse import Namespace
 from ghastoolkit.codeql.cli import CodeQL
+from ghastoolkit.codeql.packs.pack import CodeQLPack
 from ghastoolkit.utils.cli import CommandLine
+
+
+def codeqlPackPublish(arguments: Namespace):
+    if not arguments.pack or arguments.pack == "":
+        logging.error(f"CodeQL Pack path must be provided")
+        exit(1)
+
+    pack = CodeQLPack(arguments.pack)
+    # logging.info(f"CodeQL Pack :: {pack}")
+
+    remote = pack.remote_version
+
+    if pack.version != remote:
+        logging.info("Publishing CodeQL Pack...")
+        pack.publish()
+        logging.info(f"CodeQL Pack published :: {pack}")
+    else:
+        logging.info(f"CodeQL Pack is up to date :: {pack}")
 
 
 class CodeQLCommandLine(CommandLine):
@@ -11,11 +30,12 @@ class CodeQLCommandLine(CommandLine):
     def arguments(self):
         """CodeQL arguments."""
         if self.subparser:
-            self.addModes(["init", "analyze", "update"])
+            self.addModes(["init", "analyze", "update", "pack-publish"])
 
             parser = self.parser.add_argument_group("codeql")
             parser.add_argument("-b", "--binary")
             parser.add_argument("-c", "--command", type=str)
+            parser.add_argument("--pack", type=str, help="CodeQL Pack Path")
 
     def run(self, arguments: Namespace):
         codeql = CodeQL()
@@ -25,6 +45,9 @@ class CodeQLCommandLine(CommandLine):
             exit(1)
 
         logging.info(f"Found codeql on system :: '{' '.join(codeql.path_binary)}'")
+
+        if arguments.mode == "pack-publish":
+            codeqlPackPublish(arguments)
 
 
 if __name__ == "__main__":
