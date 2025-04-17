9beb54b815d0a8704970d7ec506461b3de346e37
GeekMasher <geekmasher@gmail.com>
2025-04-17 16:28:41 +0100

    feat(supplychain): Move Dependency into sepeate file

---
+++ src/ghastoolkit/supplychain/__init__.py
@@ -0,0 +1,4 @@
+from .advisories import Advisory, Advisories, AdvisoryAffect
+from .dependencies import Dependencies, uniqueDependencies
+from .dependency import Dependency
+from .licensing import Licenses
